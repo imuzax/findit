3c272ff257b216e3e2d5e95c4d96df476608e9df
Gufran Sayyed <gufransayyed324@gmail.com>
2026-04-18 06:51:45 +0530

    🐛 Fix MySQL connection by specifying port 3307

--- includes/config.php
+++ includes/config.php
@@ -4,7 +4,7 @@
  */
 
 // Database Credentials
-define('DB_HOST', 'localhost');
+define('DB_HOST', '127.0.0.1');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'findit_db');
@@ -12,7 +12,7 @@ define('DB_NAME', 'findit_db');
 // Connect to Database using PDO
 try {
     $pdo = new PDO(
-        "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8mb4",
+        "mysql:host=" . DB_HOST . ";port=3307;dbname=" . DB_NAME . ";charset=utf8mb4",
         DB_USER, 
         DB_PASS,
         [
